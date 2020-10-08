cede99a6c55d080d87e99d26daddefad5d3a1648
olivier <olivier.laffon@epitech.eu>
2020-10-08 15:55:54 +0200

    modifications

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
 const express = require('express');
 const app = express();
 
-
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({​​​​​ extended: true }​​​​​)); // support encoded bodies
 const greet = express.Router();
-greet.get('/', (req, res) => {
+
+greet.get('/', (req, res) => {​​​​​
   console.log(req.baseUrl);
   res.send('Hello World');
-})
-
-const subscribe = express.Router();
-subscribe.get('/', (req, res) => {
-  console.log(req.baseUrl);
-  res.send('Hello subscribe');
-})
-
-
-
-
+}​​​​​)
 
 app.use('/greet', greet);
 
-app.use('/subscribe', subscribe);
-
+app.post('/subscribe', function(req, res) {​​​​​
+  console.log(req.body);
+  res.send(req.body);
+}​​​​​);
 
 const port = process.env.PORT || 80;
 
-app.listen(port, err => {
-    if(err) throw err;
-    console.log("%c Server running", "color: green");
-});
+app.listen(port, err => {​​​​​
+  if(err) throw err;
+  console.log("%c Server running", "color: green");
+}​​​​​);
