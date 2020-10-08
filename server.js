c91ee25862798ae3a6e7716bb804dcd0fadd2f1a
olivier <olivier.laffon@epitech.eu>
2020-10-08 16:25:55 +0200

    modified server.js added vapidkeys and modified post request

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const app = express();
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
-const greet = express.Router();
-
-greet.get('/', (req, res) => {
-  console.log(req.baseUrl);
-  res.send('Hello World');
-})
-
-app.use('/greet', greet);
-
-app.post('/subscribe', function(req, res) {
-  console.log(req.body);
-  res.send(req.body);
-});
-
-const port = process.env.PORT || 80;
-
-app.listen(port, err => {
-  if(err) throw err;
-  console.log("%c Server running", "color: green");
-});
+const webpush = require('web-push');
+const vapidKeys = {
+	"publicKey": "BAIidyZj45O6wr5W1O2NR9nir_HXR-yBQsRgnm-Z0Un-Gf0wXvUYZ91mpkgplWKVG-IXRAayT-HrKXvTQyRFj9w",
+	"privateKey": "IPXkGY1K951VYHWaOucfjitopWB1bR7au2SjsDSSNx4"
+};
+webpush.setVapidDetails(
+  'mailto:example@yourdomain.org',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+  );
+  var bodyParser = require('body-parser');
+  app.use(bodyParser.json()); // support json encoded bodies
+  app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+  const greet = express.Router();
+  greet.get('/', (req, res) => {
+    console.log(req.baseUrl);
+    res.send('Hello World');
+  })
+  app.use('/greet', greet);
+  app.post('/subscribe', (req, res) => {​​​​​
+    const subscription = req.body;
+    res.status(201).json({​​​​​}​​​​​);
+    const payload = JSON.stringify({​​​​​ title: 'test' }​​​​​);
+  
+    console.log(subscription);
+  
+    webpush.sendNotification(subscription, payload).catch(error => {​​​​​
+      console.error(error.stack);
+    }​​​​​);
+  }​​​​​);
+  const port = process.env.PORT || 80;
+  app.listen(port, err => {
+    if(err) throw err;
+    console.log("%c Server running", "color: green");
+  });
