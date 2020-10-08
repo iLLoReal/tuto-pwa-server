e798720203c8b1704fd1bd884447b6c3eecf42f9
olivier <olivier.laffon@epitech.eu>
2020-10-08 16:04:02 +0200

    removed special chars

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ const app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({​​​​​ extended: true }​​​​​)); // support encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 const greet = express.Router();
 
-greet.get('/', (req, res) => {​​​​​
+greet.get('/', (req, res) => {
   console.log(req.baseUrl);
   res.send('Hello World');
-}​​​​​)
+})
 
 app.use('/greet', greet);
 
-app.post('/subscribe', function(req, res) {​​​​​
+app.post('/subscribe', function(req, res) {
   console.log(req.body);
   res.send(req.body);
-}​​​​​);
+});
 
 const port = process.env.PORT || 80;
 
-app.listen(port, err => {​​​​​
+app.listen(port, err => {
   if(err) throw err;
   console.log("%c Server running", "color: green");
-}​​​​​);
+});
